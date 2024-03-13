274b0ae4ac44445e10805011f41b00aa07b1e7d1
ripon4521 <riponakondo4521@gmail.com>
2024-03-14 03:23:08 +0600

    created a user dashbaord menu bar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from '@clerk/nextjs'
 import "../globals.css";
+import LeftSideBar from "@/components/layout/LeftSideBar";
+import TopBar from "@/components/layout/TopBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +20,13 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <div className="flex  max-lg:flex-col text-gray-600">
+        <LeftSideBar></LeftSideBar>
+        <TopBar></TopBar>
+    <div className="flex-1">
+    <body className={inter.className}>{children}</body>
+    </div>
+        </div>
       </html>
     </ClerkProvider>
 
--- src/components/layout/LeftSideBar.tsx
+++ src/components/layout/LeftSideBar.tsx
@@ -1,11 +1,40 @@
-import React from 'react'
+"use client"
+
+import { UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { navLinks } from "../../../lib/constanse";
+
+
 
 const LeftSideBar = () => {
-  return (
-    <div>
-  
-    </div>
-  )
-}
+  const pathname = usePathname();
 
-export default LeftSideBar
+  return (
+    <div className="h-screen left-0 top-0 sticky p-10 flex flex-col gap-16 bg-blue-200 shadow-xl max-lg:hidden">
+      <Image src="/logo.png" alt="logo" width={150} height={70} />
+
+      <div className="flex flex-col gap-12">
+        {navLinks.map((link) => (
+          <Link
+            href={link.url}
+            key={link.label}
+            className={`flex gap-4 text-body-medium ${
+              pathname === link.url ? "text-blue-600" : "text-gray-600"
+            }`}
+          >
+            {link.icon} <p>{link.label}</p>
+          </Link>
+        ))}
+      </div>
+
+      <div className="flex gap-4 text-body-medium items-center">
+        <UserButton />
+        <p>Edit Profile</p>
+      </div>
+    </div>
+  );
+};
+
+export default LeftSideBar;

963e01ed400d464b8e34f90181668e1428030da4
ripon4521 <riponakondo4521@gmail.com>
2024-03-15 16:21:57 +0600

    implement the cloudnary and image upload system

--- src/components/collections/CollectionFrom.tsx
+++ src/components/collections/CollectionFrom.tsx
@@ -6,16 +6,21 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { Button } from "@/components/ui/button"
 import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Textarea } from "../ui/textarea";
+import ImageUpload from "../customUi/ImageUpload";
+import { useRouter } from "next/navigation";
+// import { useRouter } from 'next/router';
+
+
 
 const formSchema = z.object({
     title: z.string().min(2).max(20),
@@ -23,21 +28,22 @@ const formSchema = z.object({
     image: z.string(),
 })
 const CollectionFrom = () => {
+    const router = useRouter();
 
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
-          title: "",
-          description:"",
-          image:"",
+            title: "",
+            description: "",
+            image: "",
         },
-      })
+    })
 
 
-      const onSubmit = async (values: z.infer<typeof formSchema>) =>{
-    
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+
         console.log(values)
-      }
+    }
 
 
 
@@ -49,57 +55,64 @@ const CollectionFrom = () => {
             <Separator className="bg-gray-400 my-4"></Separator>
 
             <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="title"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Title</FormLabel>
-              <FormControl>
-                <Input placeholder="Title" {...field} />
-              </FormControl>
-            
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                    <FormField
+                        control={form.control}
+                        name="title"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Title</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="Title" {...field} />
+                                </FormControl>
 
-         <FormField
-          control={form.control}
-          name="description"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Description</FormLabel>
-              <FormControl>
-              <Textarea placeholder="Description" {...field} rows={5}></Textarea>
-              
-              </FormControl>
-           
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
 
-         <FormField
-          control={form.control}
-          name="image"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Image</FormLabel>
-              <FormControl>
-             
-              
-              </FormControl>
-           
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-     
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
+                    <FormField
+                        control={form.control}
+                        name="description"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Description</FormLabel>
+                                <FormControl>
+                                    <Textarea placeholder="Description" {...field} rows={5}></Textarea>
+
+                                </FormControl>
+
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+
+                    <FormField
+                        control={form.control}
+                        name="image"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Image</FormLabel>
+                                <FormControl>
+                                    <ImageUpload value={field.value ? [field.value] : []} onChange={(url) => field.onChange(url)} onRemove={() => field.onChange("")} ></ImageUpload>
+
+                                </FormControl>
+
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+
+                    <Button type="submit">Submit</Button>
+                    <Button
+                        type="button"
+                        onClick={() => router.push("/collections")}
+                        className="bg-blue-1 text-white"
+                    >
+                        Discard
+                    </Button>
+                </form>
+            </Form>
 
         </div>
     );
